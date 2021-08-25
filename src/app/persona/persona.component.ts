f6f25d787e6e7a511da7daff5eeb9884721ebd0d
ldcarrillou <ldcarrillou@hotmail.com>
2021-08-24 21:55:26 -0500

    Aplicación Front End en Angular para Coomeva

---
+++ src/app/persona/persona.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+import { PersonaService } from './persona.service';
+
+@Component({
+  selector: 'app-persona',
+  templateUrl: './persona.component.html',
+  styleUrls: ['./persona.component.css'],
+  providers: [PersonaService]
+})
+export class PersonaComponent implements OnInit {
+
+  constructor(private personaService: PersonaService) { }
+
+  ngOnInit(): void {
+  }
+
+}
