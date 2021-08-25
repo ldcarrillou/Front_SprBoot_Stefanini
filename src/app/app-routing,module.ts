59b04bf1e845012898339bb6a25c0b96f1029c10
ldcarrillou <ldcarrillou@hotmail.com>
2021-08-25 08:51:12 -0500

    Listar Personas creado

--- src/app/app-routing,module.ts
+++ src/app/app-routing,module.ts
@@ -1,10 +1,13 @@
-import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AppComponent } from './app.component';
+import { PersonaComponent } from './persona/persona.component';
+
 const routes: Routes = [
-  { path: '', redirectTo: '/appComponent', pathMatch: 'full' },
-  { path: 'appComponent', component: AppComponent }
+  { path: '', redirectTo: '/personaComponent', pathMatch: 'full' },
+  { path: 'appComponent', component: AppComponent },
+  { path: 'personaComponent', component: PersonaComponent },
 ];
 
 @NgModule({
